f6622314b72ee3e2b020d8d75deb2628a4747873
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-07-18 07:16:38 -0400

    Add model sorting by name or date option

--- models/get_api_models.py
+++ models/get_api_models.py
@@ -12,6 +12,9 @@ import requests
 # Define California timezone
 california_tz = pytz.timezone('America/Los_Angeles')
 
+# Sort by name (True) or date (False) - default is date
+SORT_BY_NAME = False
+
 api_key = os.getenv("OPENAI_API_KEY")
 headers = {'Authorization': f'Bearer {api_key}'}
 
@@ -19,6 +22,13 @@ response = requests.get('https://api.openai.com/v1/models', headers=headers)
 
 if response.status_code == 200:
     models = response.json()
+    
+    # Sort models based on the switch
+    if SORT_BY_NAME:
+        models['data'].sort(key=lambda x: x['id'])
+    else:
+        models['data'].sort(key=lambda x: x['created'])
+    
     for model in models['data']:
         timestamp = model['created']
         # Convert UTC timestamp to local California time
